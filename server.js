809899b1eb0d1affb4ccadb83202ff982abf26db
mruldig <mruldig@gmail.com>
2023-08-08 17:53:55 -0700

    another new action

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from /test Node!';
+  const msg = 'Hello from Node!';
   res.send(getPage(msg));
 });
 
